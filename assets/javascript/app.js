ef8241a1a04ad8f63178240b08cf1e3d0c47855b
Barchard <lb185070@ncr.com>
2017-03-16 07:23:39 -0400

    have basic logic to determine winner

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,16 +11,50 @@ $(document).ready(function() {
 
     //Initial variables***************************************************************
     var database = firebase.database();
-    var auth = firebase.auth();
-
-    //Functions**********************************************************
-    function assignToGame() {
-        console.log('Button Clicked');
-
-        
+    
+    // Initial Values
+    var player1 = "Player 1";
+    var player2 = "Player 2";
+    var player1Choice = "scissors";
+    var player2Choice = "scissors";
+    var player1Wins = "0";
+    var player2Wins = "0";
+    var winner = "";
+    var game = {
+        player1: player1,
+        player2: player2,
+        player1Choice: player1Choice,
+        player2Choice: player2Choice,
+        player1Wins: player1Wins,
+        player2Wins: player2Wins
     }
+    
+    //Functions**********************************************************
+    function findWinner(player1Choice, player2Choice) {
+        if (player1Choice === player2Choice) {
+            winner = "tie"
+        }
+        else if ((player1Choice === "rock") & (player2Choice === "scissors") || 
+        (player1Choice === "scissors") & (player2Choice === "paper") ||
+        (player1Choice === "paper") & (player2Choice === "rock")) {
+            winner = player1
+        }
+        else {
+            winner = player2
+        }
+
+        database.ref().set({
+            winner: winner,
+        });
+    }
+
     //Event listeners***************************************************************
-    $("#begin-game").click(assignToGame)
+    $("#begin-game").click(function(assignToGame) {
+        assignToGame.preventDefault();
+        player1 = $("#name").val().trim();
+        console.log(player1);
+        findWinner (player1Choice, player2Choice);
+    });
     //Startup routine***************************************************************
     
 });
